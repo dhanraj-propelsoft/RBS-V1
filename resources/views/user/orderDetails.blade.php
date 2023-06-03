6117a5c4bc038b702d2875767582f6ef3424b5f3
VallatharasuPropelSoft <vallatharasu@propelsoft.in>
2023-06-03 14:12:19 +0530

    .

--- resources/views/user/orderDetails.blade.php
+++ resources/views/user/orderDetails.blade.php
@@ -228,7 +228,7 @@
 
 							<div class="form-group clearfix">
 								<a href="javascript:;" class="form-wizard-previous-btn btn btn-secondary float-start">Previous</a>
-								<a href="javascript:;" class="form-wizard-submit float-end btn btn-primary">Submit</a>
+								<button type="submit" class="form-wizard-submit float-end btn btn-primary">Submit</button>
 							</div>
 						</fieldset>
 
@@ -237,132 +237,12 @@
 			</div>
 		</div>
 	</section>
-	<div class="card col-md-6 m-auto p-3 d-none order-confirm" style="height:100%;">
-
-		{{-- --}}
-
-
-		<div class="card-header d-flex align-items-center">		
-			<h5 class="text-center w-100">Order Details</h5>
-			<div style="width:80px;">
-			  <button type="button" class="border-0 bg-white revert-order-details">
-				  <i class="fas fa-edit ml-2" ></i>
-				  </button>			
-			  <button type="button" class="btn-close"  aria-label="Close"></button>
-		  </div>  
-			
-			  
-		  </div>
-		  <div class="card-body">
-		  </div>
-	  
-		  <div class="row">
-			<h5 class="col-md-6">Date & Time of Supply:</h5>
-			<p class="col-md-6" data-from="datetime">12/08/2023, 12:00 pm</p>
-		  </div>
-		  <div class="row">
-			<h5 class="col-md-6" >Site Address:</h5>
-			<p class="col-md-6" data-from="siteAddresses">westcross street, Trichy</p>
-		  </div>
-		  <div class="row">
-			<h5 class="col-md-6" >Billing Address:</h5>
-			<p class="col-md-6" data-from="billAddresses">propelsoft, BDU Campus</p>
-		  </div>
-		  <div class="row">
-			<h5 class="col-md-6">Concrete Grade:</h5>
-			<p class="col-md-6"  data-from="conGrade">Grade: B</p>
-		  </div>
-		  <div class="row">
-			<h5 class="col-md-6 " >Quantity in cubic meter:</h5>
-			<p class="col-md-6" data-from="quantity">32</p>
-		  </div>
-		  <div class="row">
-			<h5 class="col-md-6" >Service:</h5>
-			<p class="col-md-6" data-from="service">32</p>
-		  </div>
-		  <div class="row">
-			<h5 class="col-md-6" >Net Amount:</h5>
-			<p class="col-md-6" data-from="netAmount">13:00</p>
-		  </div>
-		  <div class="row">
-			<h5 class="col-md-6" >Advance:</h5>
-			<p class="col-md-6" data-from="advance">13:00</p>
-		  </div>
-		  <div class="row">
-			<h5 class="col-md-6" >Remark:</h5>
-			<p class="col-md-6" data-from="remarks">this is remark</p>
-		  </div>
-			<a href="#" class="btn btn-primary col-12 col-md-6 col-lg-6 m-auto order_confirm" >Order Confirm</a>
-		  </div>
-		</div>
-	</div>
-  
+	
 
 
 <script>
 	jQuery(document).ready(function() {
-		$(".order_confirm").click(function() {
-			var data = {};
-
-$("input ,select , textarea").each(function() {
-  var name = $(this).attr("name");
-  console.log(name);
-  var value = $(this).val();
-  if (!name) {
-	return;
-  }
-  if ($(this).is(":checkbox") ) {
-	if ( $(this).is(":checked")) {
-		if (data[name] && Array.isArray(data[name])) {
-      data[name].push(value);
-    } else {
-      data[name] = [value]; // Create a new array with the checkbox value
-    }
-	}
-   
-  } else {
-	data[name] = value;
-  }
-});
-
-console.log(data); 
-
-			let timerInterval
-			const swalWithBootstrapButtons = Swal.mixin({
-				customClass: {
-					confirmButton: 'btn btn-primary',
-
-				},
-				buttonsStyling: false
-			})
-			swalWithBootstrapButtons.fire({
-				confirmButtonText: 'Go to Home',
-
-				html: 'ThankYou for  becoming our valuable customer .Our support team will contact you  shortly.',
-				timer: 4000,
-				timerProgressBar: true,
-				didOpen: () => {
-					// Swal.showLoading()
-					// const b = Swal.getHtmlContainer().querySelector('b')
-					timerInterval = setInterval(() => {
-						b.textContent = Swal.getTimerLeft()
-					}, 100)	
-				},
-				willClose: () => {
-					clearInterval(timerInterval)
-				}
-			}).then((result) => {
-				/* Read more about handling dismissals below */
-				if (result.dismiss === Swal.DismissReason.timer || result.isConfirmed === true) {
-					window.history.pushState(null, null, "/");
-					window.location.replace("/");
-					// Add a new history entry with a different URL
-
-				}
-
-			})
-		});
-		// click on next button
+		
 		jQuery('.form-wizard-next-btn').click(function() {
 			var parentFieldset = jQuery(this).parents('.wizard-fieldset');
 			var currentActiveStep = jQuery(this).parents('.form-wizard').find('.form-wizard-steps .active');
